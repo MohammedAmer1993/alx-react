feede2f4e17d682d08e607d491f7d5c02efd8a80
Mohammed Amer <loard.amer.2010@gmail.com>
2025-01-20 01:15:12 +0200

    react intro task 1

---
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,24 @@
+import alxLogo from "./alxlogo.jpg";
+import "./App.css";
+import { getFullYear, getFooterCopy } from "./utils";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={alxLogo} alt="the logo" />
+        <p>School dashboard</p>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(true)}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
