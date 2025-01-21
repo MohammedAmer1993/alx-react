fbcf24d85eca2f7eec7f29a40124500c2d0d59f3
Mohammed Amer <loard.amer.2010@gmail.com>
2025-01-21 23:07:59 +0200

    task 4 deployment

---
+++ 0x01-react_intro/task_4/dashboard/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  moduleFileExtensions: [
+    "web.js",
+    "js",
+    "web.ts",
+    "ts",
+    "web.tsx",
+    "tsx",
+    "json",
+    "web.jsx",
+    "jsx",
+    "node",
+  ],
+};
