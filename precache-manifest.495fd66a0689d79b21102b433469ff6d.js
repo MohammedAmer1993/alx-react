dbcea3af137b59af1f8aa179535373328b4cc07d
Mohammed Amer <loard.amer.2010@gmail.com>
2025-01-21 23:09:22 +0200

    Updates

---
+++ precache-manifest.495fd66a0689d79b21102b433469ff6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b18c837bb54614f62c88ac7ad9f4630",
+    "url": "/alx-react/0x01-react_intro/task_4/dashboard/index.html"
+  },
+  {
+    "revision": "573859ab72cedc4808c3",
+    "url": "/alx-react/0x01-react_intro/task_4/dashboard/static/css/main.b2b84028.chunk.css"
+  },
+  {
+    "revision": "51bd97abbf0a41a84552",
+    "url": "/alx-react/0x01-react_intro/task_4/dashboard/static/js/2.82eedec8.chunk.js"
+  },
+  {
+    "revision": "573859ab72cedc4808c3",
+    "url": "/alx-react/0x01-react_intro/task_4/dashboard/static/js/main.1573d97e.chunk.js"
+  },
+  {
+    "revision": "d5414931466f8a9cab48",
+    "url": "/alx-react/0x01-react_intro/task_4/dashboard/static/js/runtime~main.3d9bf963.js"
+  },
+  {
+    "revision": "23d20eb3253de8404d66401eb6db58d5",
+    "url": "/alx-react/0x01-react_intro/task_4/dashboard/static/media/alxlogo.23d20eb3.jpg"
+  }
+]);
