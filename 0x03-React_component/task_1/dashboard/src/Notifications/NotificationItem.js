07219342eed66902581ae1577d0619e833376f25
Mohammed Amer <loard.amer.2010@gmail.com>
2025-01-24 15:47:58 +0200

    task 0 1 react components

---
+++ 0x03-React_component/task_1/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,21 @@
+import "./NotificationItem.css";
+import PropTypes from "prop-types";
+
+export default function NotificationItem({ type, html, value }) {
+  return (
+    <li
+      data-testid="notificationItem"
+      data-notification-type={type}
+      dangerouslySetInnerHTML={html}
+    >
+      {value}
+    </li>
+  );
+}
+NotificationItem.propTypes = {
+  html: PropTypes.shape({
+    __html: PropTypes.string.isRequired,
+  }),
+  type: PropTypes.string.isRequired,
+  value: PropTypes.string,
+};
